963486c979066dce91bfef6a3a94b82bbf9e54d4
DESKTOP-9BU7D29\ㄉㄉ <x60346@gmail.com>
2026-03-28 11:14:53 +0800

    fix: i18n > 修正 en-US、zh-TW語言檔不對稱問題

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -11,7 +11,7 @@ export default {
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       },
     },
-    skill: {},
-    timeline: {},
+    skill: '',
+    timeline: '',
   },
 };
--- src/i18n/zh-TW/index.ts
+++ src/i18n/zh-TW/index.ts
@@ -2,6 +2,16 @@
 // so you can safely delete all default props below
 
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful',
+  store: {
+    project: {
+      project01: {
+        title: 'Lorem ipsum',
+        cover: 'https://cdn.quasar.dev/img/parallax2.jpg',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+    },
+    skill: '',
+    timeline: '',
+  },
 };
